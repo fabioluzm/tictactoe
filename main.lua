510040a1985109b0e7d528703df18152f8c37a3a
fabioluzm <fa.moreira23@gmail.com>
2019-04-02 00:31:50 +0100

    add font, keyspressed, and draw X and O on player turns

--- main.lua
+++ main.lua
@@ -20,9 +20,14 @@ WINDOW_HEIGHT = 720
 
 GRID_HEIGHT, GRID_WIDTH = 3, 3
 
+GRID_TILE_SIZE = 40
+
+SPRITE_PADDING = 4
 --[[ assets ]]
 local xSprite = love.graphics.newImage('graphics/x.png')
 local oSprite = love.graphics.newImage('graphics/o.png')
+
+local retroFont = love.graphics.newFont('fonts/pressstart.ttf', 8)
 
 --[[ data structures ]]
 local grid = {
@@ -31,9 +36,15 @@ local grid = {
 	{"", "", ""}
 }
 
+local currentPlayer = 1
+local selectedX, selectedY = 1, 1
+
 function love.load()
 	-- remove smoothness from rendering, giving a more aesthethic retro aspect
 	love.graphics.setDefaultFilter('nearest','nearest')
+	love.graphics.setFont(retroFont)
+
+	love.window.setTitle('TicTacToe')
 
 	-- setup  game screen based on virtual resolution
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -47,6 +58,34 @@ function love.keypressed(key)
 	if key == 'escape' then
 		love.event.quit()
 	end
+
+	if key == 'left' or key == 'a'then
+		if selectedX > 1 then
+			selectedX = selectedX - 1
+		end
+	elseif key == 'right' or key == 'd' then
+		if selectedX < 3 then
+			selectedX = selectedX + 1
+		end
+	elseif key == 'up'or key == 'w' then
+		if selectedY > 1 then
+			selectedY = selectedY - 1
+		end
+	elseif key == 'down' or key == 's' then
+		if selectedY < 3 then
+			selectedY = selectedY + 1
+		end
+	elseif key == 'space' or key == 'enter' then
+		if grid[selectedY][selectedX] == "" then
+			if currentPlayer == 1 then
+				grid[selectedY][selectedX] = 'X'
+				currentPlayer = 2
+			else
+				grid[selectedY][selectedX] = 'O'
+				currentPlayer = 1
+			end
+		end
+	end
 end
 
 function love.update(dt)
@@ -56,24 +95,52 @@ end
 function love.draw()
 	push:start()
 	drawGrid()
+	love.graphics.print('Player ' .. currentPlayer .. "'s turn", 1, 1)
 	push:finish()
 end
 
 function drawGrid()
+	-- calculate margins
+	local xMargin = VIRTUAL_WIDTH - (GRID_TILE_SIZE * GRID_WIDTH)
+	local yMargin =  VIRTUAL_HEIGHT - (GRID_TILE_SIZE * GRID_HEIGHT)
 
-	-- draw lines of the grid
+	love.graphics.setLineWidth(2)
+	
+	--[[ draw lines of the grid ]]
+	
+	-- vertical lines
+	love.graphics.line(xMargin/2 + GRID_TILE_SIZE, yMargin/2,
+		xMargin/2 + GRID_TILE_SIZE, VIRTUAL_HEIGHT - yMargin/2)
+	
+	love.graphics.line(VIRTUAL_WIDTH - xMargin/2 - GRID_TILE_SIZE, yMargin/2,
+		VIRTUAL_WIDTH - xMargin/2 - GRID_TILE_SIZE, VIRTUAL_HEIGHT - yMargin/2)
+	
+	-- horizontal lines
+	love.graphics.line(xMargin/2, yMargin/2 + GRID_TILE_SIZE,
+		VIRTUAL_WIDTH - xMargin/2, yMargin/2 + GRID_TILE_SIZE)
 
+	love.graphics.line(xMargin/2, VIRTUAL_HEIGHT - yMargin/2 - GRID_TILE_SIZE,
+		VIRTUAL_WIDTH - xMargin/2, VIRTUAL_HEIGHT - yMargin/2 - GRID_TILE_SIZE)
+	
+	-- draw the sprites within the lines
 	for y = 1, GRID_HEIGHT do
 		for x = 1, GRID_WIDTH do
+			local xOffset = xMargin/2 + GRID_TILE_SIZE * (x - 1)
+			local yOffset = yMargin/2 + GRID_TILE_SIZE * (y - 1)
 			if grid[y][x] == "" then
 				-- draw nothing
 			elseif grid [y][x] == "X" then
 				-- draw X sprite at x * y location
-				love.graphics.draw(xSprite)
+				love.graphics.draw(xSprite, xOffset + SPRITE_PADDING, yOffset + SPRITE_PADDING)
 			else
 				-- draw O sprite at x * Y location
-				love.graphics.draw(oSprite)
-			end 
+				love.graphics.draw(oSprite, xOffset + SPRITE_PADDING, yOffset + SPRITE_PADDING)
+			end
+			if x == selectedX and y == selectedY then
+				love.graphics.setColor(1, 1, 1, 0.5)
+				love.graphics.rectangle('fill', xOffset, yOffset, GRID_TILE_SIZE, GRID_TILE_SIZE)
+				love.graphics.setColor(1, 1, 1, 1)
+			end
 		end
 	end
 end
